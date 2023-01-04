cb56aff9a236a2564db7035180b620bfba6d31b8
Vishal Panwar <119125256+vishallpanwar@users.noreply.github.com>
2023-01-04 18:06:31 +0530

    Add files via upload

---
+++ Inheritance/ProtectedToPublicInheritance.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+
+class human{             
+
+    protected:
+    int height;
+
+    public:
+    int weight;
+
+    private:
+    int age;
+
+    public:
+
+    int getAge(){
+        return this->age;
+    }
+
+    void setWeight(int w){
+        this->weight=w;
+    }
+};
+
+    // Male is child class
+
+    class male:public human{            
+
+        public:
+
+        string colour;
+
+        void sleep(){
+            cout<<"Male is sleeping";
+        }
+};
+
+int main(){
+
+    male m1;
+
+    // Ye bhi private hojyega, to access it, getter and setter ka use krna pdega
+    cout<<m1.height;
+    
+    return 0;
+}
